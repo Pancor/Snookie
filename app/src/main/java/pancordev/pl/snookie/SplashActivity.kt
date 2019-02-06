e80b001c4d293f60be26ef9ab27bbb4613f6dbfc
Pawel <pancordev@gmail.com>
2019-02-06 17:52:38 +0100

    implemented launcher theme

--- app/src/main/java/pancordev/pl/snookie/SplashActivity.kt
+++ app/src/main/java/pancordev/pl/snookie/SplashActivity.kt
@@ -7,6 +7,6 @@ class SplashActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.act_splash)
+
     }
 }
