12343bf527284dcc1d3bb1a9708b0421ac925784
Pawel <pancordev@gmail.com>
2019-02-06 17:01:13 +0100

    created SplashActivity

---
+++ app/src/main/java/pancordev/pl/snookie/SplashActivity.kt
@@ -0,0 +1,12 @@
+package pancordev.pl.snookie
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class SplashActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_splash)
+    }
+}
