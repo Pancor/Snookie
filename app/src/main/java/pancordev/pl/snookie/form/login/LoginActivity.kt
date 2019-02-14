161bbb089b535a063b6cca9b6030943bbbb1ad8d
Pawel <pancordev@gmail.com>
2019-02-14 19:01:11 +0100

    included login xml to activity

--- app/src/main/java/pancordev/pl/snookie/form/login/LoginActivity.kt
+++ app/src/main/java/pancordev/pl/snookie/form/login/LoginActivity.kt
@@ -1,5 +1,7 @@
 package pancordev.pl.snookie.form.login
 
+import android.os.Bundle
+import pancordev.pl.snookie.R
 import pancordev.pl.snookie.base.BaseActivity
 import javax.inject.Inject
 
@@ -8,6 +10,12 @@ class LoginActivity: BaseActivity(), LoginContract.View {
 
     @Inject
     lateinit var presenter: LoginContract.Presenter
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.act_login)
+        
+    }
 
     override fun signedIn() {
         TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
