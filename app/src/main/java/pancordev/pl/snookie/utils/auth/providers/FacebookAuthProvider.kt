4de1f2e95a812a22611ab8606dceaa1c3608156a
Pawel <pancordev@gmail.com>
2019-04-14 22:23:44 +0200

    added the simplest fb sign in

--- app/src/main/java/pancordev/pl/snookie/utils/auth/providers/FacebookAuthProvider.kt
+++ app/src/main/java/pancordev/pl/snookie/utils/auth/providers/FacebookAuthProvider.kt
@@ -1,19 +1,19 @@
 package pancordev.pl.snookie.utils.auth.providers
 
 import android.app.Activity
-import android.content.Context
 import com.facebook.CallbackManager
 import com.facebook.FacebookCallback
 import com.facebook.FacebookException
 import com.facebook.login.LoginManager
 import com.facebook.login.LoginResult
+import com.google.firebase.auth.FacebookAuthProvider
 import com.google.firebase.auth.FirebaseAuth
 import io.reactivex.Single
 import pancordev.pl.snookie.di.ActivityScoped
 import pancordev.pl.snookie.model.Result
 import pancordev.pl.snookie.utils.auth.AuthContract
+import pancordev.pl.snookie.utils.auth.AuthManager
 import javax.inject.Inject
-import javax.inject.Singleton
 
 @ActivityScoped
 class FacebookAuthProvider @Inject constructor(private val auth: FirebaseAuth,
@@ -22,24 +22,30 @@ class FacebookAuthProvider @Inject constructor(private val auth: FirebaseAuth,
                                                private val activity: Activity) : AuthContract.Facebook {
 
     override fun signIn(): Single<Result> {
-        //loginManager.logInWithReadPermissions(activity, arrayListOf("email", "public_profile"))
+        return Single.create { emitter ->
+            loginManager.logInWithReadPermissions(activity, arrayListOf("email", "public_profile"))
+            loginManager.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
+                override fun onSuccess(result: LoginResult) {
+                    val accessToken = result.accessToken
+                    val credential = FacebookAuthProvider.getCredential(accessToken.token)
+                    auth.signInWithCredential(credential)
+                        .addOnCompleteListener { result ->
+                            if (result.isSuccessful) {
+                                emitter.onSuccess(Result(isSucceed = true, code = AuthManager.SIGN_IN_SUCCEED))
+                            }
+                        }
 
+                }
 
-        loginManager.registerCallback(callbackManager, object: FacebookCallback<LoginResult> {
-            override fun onSuccess(result: LoginResult) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
+                override fun onCancel() {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
 
-            override fun onCancel() {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-
-            override fun onError(error: FacebookException) {
-                TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-            }
-        })
-
-        return Single.just(Result(true, ""))
+                override fun onError(error: FacebookException) {
+                    TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+                }
+            })
+        }
     }
 
     override fun signUp(): Single<Result> {
